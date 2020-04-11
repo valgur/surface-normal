b55a939aa959303b51c90a3d1974990c4a23d521
Martin Valgur <martin.valgur@gmail.com>
2020-04-11 22:07:41 +0300

    revert back to using OpenCV for IO, but keep NumPy bindings
    Processing the full KITTI dataset was 15x slower when using the NumPy-based Python bindings.

--- src/python.cpp
+++ src/python.cpp
@@ -1,31 +1,37 @@
 #include <string>
-#include <tuple>
 
-#include <pybind11/numpy.h>
 #include <pybind11/pybind11.h>
 
 #include "surface_normal/numpy.hpp"
+#include "surface_normal/opencv2.hpp"
 
 namespace py = pybind11;
 using namespace surface_normal;
 
-using CameraIntrinsicsTuple = std::tuple<double, double, double>;
+void normals_from_depth_imgfile_wrapper(const std::string &depth_in_path,
+                                        const std::string &normals_out_path,
+                                        CameraIntrinsicsTuple intrinsics_tuple, int window_size,
+                                        float max_rel_depth_diff, bool use_cuda) {
+  normals_from_depth_imgfile(depth_in_path, normals_out_path, intrinsics_tuple, window_size,
+                             max_rel_depth_diff, use_cuda);
+}
 
-py::array_t<uint8_t> normals_from_depth_wrapper(const py::array &depth,
-                                                CameraIntrinsicsTuple intrinsics_tuple,
-                                                int window_size, float max_rel_depth_diff,
-                                                bool use_cuda) {
-  CameraIntrinsics intrinsics{};
-  intrinsics.f  = std::get<0>(intrinsics_tuple);
-  intrinsics.cx = std::get<1>(intrinsics_tuple);
-  intrinsics.cy = std::get<2>(intrinsics_tuple);
-  return normals_from_depth(depth, intrinsics, window_size, max_rel_depth_diff, use_cuda);
+py::array_t<uint8_t> normals_from_depth_numpy_wrapper(const py::array &depth,
+                                                      CameraIntrinsicsTuple intrinsics_tuple,
+                                                      int window_size, float max_rel_depth_diff,
+                                                      bool use_cuda) {
+  return normals_from_depth(depth, intrinsics_tuple, window_size, max_rel_depth_diff, use_cuda);
 }
 
 PYBIND11_MODULE(surface_normal, m) {
   m.doc() = "";
-  m.def("normals_from_depth", &normals_from_depth_wrapper, py::arg("depth"), py::arg("intrinsics"),
-        py::arg("window_size") = 15, py::arg("max_rel_depth_diff") = 0.1,
+
+  m.def("normals_from_depth", &normals_from_depth_imgfile_wrapper, py::arg("depth_in_path"),
+        py::arg("normals_out_path"), py::arg("intrinsics"), py::arg("window_size") = 15,
+        py::arg("max_rel_depth_diff") = 0.1, py::arg("use_cuda") = USE_CUDA);
+
+  m.def("normals_from_depth_numpy", &normals_from_depth_numpy_wrapper, py::arg("depth"),
+        py::arg("intrinsics"), py::arg("window_size") = 15, py::arg("max_rel_depth_diff") = 0.1,
         py::arg("use_cuda") = USE_CUDA);
 
 #ifdef VERSION_INFO
